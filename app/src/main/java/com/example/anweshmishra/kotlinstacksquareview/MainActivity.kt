aaa196700d959b230b3146bac5b5ef8b44588dff
Anwesh43 <anweshthecool0@gmail.com>
2018-05-21 10:45:56 +0530

    adding fullscreen support

--- app/src/main/java/com/example/anweshmishra/kotlinstacksquareview/MainActivity.kt
+++ app/src/main/java/com/example/anweshmishra/kotlinstacksquareview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.anweshmishra.kotlinstacksquareview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.stacksquareview.StackSquareView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        StackSquareView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
