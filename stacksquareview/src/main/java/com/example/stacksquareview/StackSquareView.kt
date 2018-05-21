33e9f284d2cd79e46ae3f3ed80dac1834deae49e
Anwesh43 <anweshthecool0@gmail.com>
2018-05-21 09:58:04 +0530

    adding StackSquareView class

--- stacksquareview/src/main/java/com/example/stacksquareview/StackSquareView.kt
+++ stacksquareview/src/main/java/com/example/stacksquareview/StackSquareView.kt
@@ -7,3 +7,23 @@ import android.content.Context
 import android.view.View
 import android.view.MotionEvent
 import android.graphics.*
+
+val SQUARES = 4
+
+class StackSquareView (ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
