a6c773e3e0aac05b25845ad92c75f565a53fbaf7
Anwesh43 <anweshthecool0@gmail.com>
2018-05-21 10:01:48 +0530

    adding State class

--- stacksquareview/src/main/java/com/example/stacksquareview/StackSquareView.kt
+++ stacksquareview/src/main/java/com/example/stacksquareview/StackSquareView.kt
@@ -26,4 +26,31 @@ class StackSquareView (ctx : Context) : View(ctx) {
         }
         return true
     }
+
+    data class State(var prevScale : Float = 0f, var dir : Float = 0f, var j : Int = 0) {
+
+        val scales : Array<Float> = arrayOf(0f, 0f)
+
+        fun update(stopcb : (Float) -> Unit) {
+            scales[j] += 0.1f * dir
+            if (Math.abs(scales[j] - prevScale) > 1) {
+                prevScale = scales[j] + dir
+                j += dir.toInt()
+                if (j == SQUARES && j == -1) {
+                    j -= dir.toInt()
+                    dir = 0f
+                    prevScale = scales[j]
+                    stopcb(prevScale)
+                }
+            }
+        }
+
+        fun startUpdating(startcb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1 - 2 * prevScale
+                startcb()
+            }
+        }
+    }
+
 }
